4a1ab9d14ac79978fbd187f137a617034f8def79
ekdnlt <ekdnlt1532@naver.com>
2021-04-08 15:02:41 +0900

    added button

--- app/src/main/java/com/example/locationproviderlist/LocationProvideListActivity.java
+++ app/src/main/java/com/example/locationproviderlist/LocationProvideListActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
@@ -12,21 +14,30 @@ public class LocationProvideListActivity extends AppCompatActivity {
     TextView mtextView;
     LocationManager lm;
     List<String> locProvlist;
+    Button mbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mtextView = findViewById(R.id.textOutput);
-        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mbutton = findViewById(R.id.button);
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lm = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-        locProvlist =lm.getAllProviders();
+                locProvlist =lm.getAllProviders();
 
-        String s = "";
-        for(int i = 0;i<locProvlist.size(); i++){
-            s += "Loc. provider: " +locProvlist.get(i) +"\n"
-                    + "Stats: " +lm.isProviderEnabled(locProvlist.get(i))+"\n\n";
-        }
-        mtextView.setText(s);
+                String s = "";
+                for(int i = 0;i<locProvlist.size(); i++){
+                    s += "Loc. provider: " +locProvlist.get(i) +"\n"
+                            + "Stats: " +lm.isProviderEnabled(locProvlist.get(i))+"\n\n";
+                }
+                mtextView.setText(s);
+            }
+        });
+
     }
 }
